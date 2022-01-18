c98efb43c013df24b0c3f8633df190defda04a6f
effattamanna09 <effat.tamanna@savethechildren.org>
2022-01-17 16:00:47 -0800

    add header and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer dark padless>
+    <v-card class="py-2 white--text text-center" height="200px"> </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+
+  data: () => ({
+   
+  }),
+};
+</script>
